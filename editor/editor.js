d4911cf413b29aa6253922b31a3366cd4583ef38
Shane Argo <shane@shaneargo.net>
2014-07-21 23:21:41 +1000

    Markdown preview.

--- editor/editor.js
+++ editor/editor.js
@@ -1,7 +1,15 @@
+marked.setOptions = {
+	gfm: true,
+	tables : true,
+	sanitize: false
+}
+
 var editor = ace.edit("editor");
 editor.getSession().setMode("ace/mode/markdown");
 
 var saveButton = $("#saveButton");
+var preview = $("#preview");
+var previewContainer = $(".previewContainer");
 
 saveButton.click(function (e){
 	saveFile(e);
@@ -39,3 +47,21 @@ function switchClass(el, rem, add) {
 	el.removeClass(rem);
 	el.addClass(add);
 }
+
+function updatePreview() {
+	preview.html(marked(editor.getValue()));
+	$('pre').each(function(i, block) {
+		hljs.highlightBlock(block);
+	});
+}
+
+function scrollPreview() {
+  var ratio = editor.getFirstVisibleRow() / (editor.getSession().getLength() - 1 - (editor.getLastVisibleRow() - editor.getFirstVisibleRow()));
+  var scroll = Math.round(ratio * (previewContainer[0].scrollHeight - previewContainer.height()))
+  previewContainer.scrollTop(scroll);
+}
+
+editor.getSession().on('change', updatePreview);
+editor.getSession().on('changeScrollTop', function() {
+	setTimeout(scrollPreview, 0);
+})
